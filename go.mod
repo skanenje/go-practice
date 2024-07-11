5a2a18fe973c5aca637ac72412013ddd12dd2093
skanenje <swapomuse@gmail.com>
2024-07-12 01:33:09 +0300

    level 5 additions

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module tri
 
-go 1.23
+go 1.22
 
 require github.com/01-edu/z01 v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,162 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"os"
-
 	"github.com/01-edu/z01"
 )
+// ####################### UNION ################################
+// func main(){
+// 	args := os.Args[1:]
+// 		if len(args) != 2 {
+// 			return
+// 		}
+//  s := args[0] + args[1]
+//   union(s)
+// }
+// func union(s string){
+// 	var noDoubles map[rune]bool = map[rune]bool{}
+
+// 	for _, v := range s {
+// 		if !noDoubles[v]{
+// 			z01.PrintRune(v)
+// 			noDoubles[v] = true
+// 		}
+// 	}
+// 	z01.PrintRune('\n')
+// }
+// ####################################3 SWAP BITS @##############################
+// func SwapBits(octet byte) byte {
+// 	// Extract the upper and lower halves
+// 	upperHalf := octet >> 4    // Shift right by 4 bits to get upper half
+// 	lowerHalf := octet & 0x0F  // Mask with 0x0F (00001111) to get lower half
+
+// 	// Swap and combine them
+// 	return (lowerHalf << 4) | upperHalf
+// }
+
+// func main() {
+// 	var octet byte = 0b01000001
+// 	swapped := SwapBits(octet)
+
+// 	fmt.Printf("Original byte: %08b\n", octet)
+// 	fmt.Printf("Swapped byte:   %08b\n", swapped)
+// }
+// ################################## PIG LATIN ####################################
+// func main(){
+// 	args := os.Args[1:]
+// 	if len(args) != 1 {
+// 		return
+// 	}
+// 	s := args[0]
+// 	if containsVow(s){
+// 		res := piglatin(s)
+// 	fmt.Println(res)
+// 	}else {
+// 		fmt.Println("No Vowels")
+// 	}
+	
+// }
+// func piglatin(s string)string{
+// 	res := ""
+// 	for i, v := range s {
+// 		if i == 0 && isVowel(v) {
+// 			res += s + "ay"
+// 		}else if isVowel(v){
+// 			res += s[i:] + s[:i] + "ay"
+// 		}
+
+// 	}
+// 	return res
+// }
+// func isVowel(r rune)bool{
+// 	if r == 'a' ||  r == 'e' ||  r == 'i' ||  r == 'o' ||  r == 'u' || r == 'A' ||  r == 'E' ||  r == 'I' ||  r == 'O' ||  r == 'U' {
+// 		return true
+// 	}
+// 	return false
+// }
+// func containsVow(s string)bool{
+// 	var vowels map[string]bool = map[string]bool{
+// 		"a": true ,
+// 		"e": true ,
+// 		"i": true ,
+// 		"o": true ,
+// 		"u": true ,
+// 		"A": true ,
+// 		"E": true ,
+// 		"I": true ,
+// 		"O": true ,
+// 		"U": true ,
+// 	}
+// 	for _, v := range s {
+// 		if vowels[string(v)]{
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+// ########################   ROMAN NUMERALS ##################################
+// type romanNumerals []struct {
+// 	value   int
+// 	numeral string
+// 	calc    string
+// }
+// func toRoman(x int) (string, string) {
+// 	romanNumerals := romanNumerals{
+// 		{1000, "M", "M"},
+// 		{900, "CM", "(M-C)"},
+// 		{500, "D", "D"},
+// 		{400, "CD", "(D-C)"},
+// 		{100, "C", "C"},
+// 		{90, "XC", "(X-C)"},
+// 		{50, "L", "L"},
+// 		{40, "XL", "(L-X)"},
+// 		{10, "X", "X"},
+// 		{9, "IX", "(X-I)"},
+// 		{5, "V", "V"},
+// 		{4, "IV", "(V-I)"},
+// 		{1, "I", "I"},
+// 	}
+
+// 	if x > 3999 {
+// 		fmt.Println("ERROR: cannot convert to roman digit")
+// 	}
+// 	result := ""
+// 	calculation := ""
+// 	for _, rn := range romanNumerals {
+// 		for x >= rn.value {
+// 			result += rn.numeral
+// 			if calculation != "" {
+// 				calculation += "+"
+// 			}
+// 			calculation += rn.calc
+// 			x -= rn.value
+// 		}
+// 	}
+// 	return result, calculation
+// }
+
+// func main() {
+// 	args := os.Args[1:]
+// 	if len(args) > 1 {
+// 		fmt.Println("ERROR: cannot convert to roman digit")
+// 	}
+// 	num, err := Atoi(args[0])
+// 	if err != nil {
+// 		fmt.Println("ERROR: cannot convert to roman digit")
+// 	} else {
+// 		if num > 3999 {
+// 			fmt.Println("ERROR: cannot convert to roman digit")
+// 		} else {
+// 			roman, calculation := toRoman(num)
+// 			fmt.Println(calculation)
+// 			fmt.Println(roman)
+// 		}
+
+// 	}
+
+// }
+// ###################   PRINT BITS    #####################################
 // func printBits(x int){
 // 	res := ""
 // 	if x == 1 {
@@ -19,18 +170,18 @@ import (
 // 			x /= 2
 // 		}
 // 	}
-	
+
 // 	for len(res) < 8 {
 // 		res += "0"
 // 		if len(res) == 8 {
 // 			break
 // 		}
-// 	}	
+// 	}
 // 	for i := len(res)-1; i >= 0; i-- {
 // 		z01.PrintRune(rune(res[i]))
 // 	}
 // 	z01.PrintRune('\n')
-	
+
 // }
 // func checkLength(s string)string{
 // 	str := s
@@ -50,22 +201,39 @@ import (
 // 	  printBits(0)
 // 	}
 // 	printBits(res)
-	// resi := checkLength(resu)
-	
+// resi := checkLength(resu)
+
 // }
-// func isPowerof(x, exp int)bool{
-// 	for x > 0{
-// 		if x%exp == 0{
-// 			return true
-// 		}
-// 		x /= exp
+// ##########################    IS POWER OF 2 ######################@@@##########@@
+// func isPowerof2(x int) bool {
+// 	if x == 1 {
+// 		return true
 // 	}
-// 	return false
+// 	for x >= 2 {
+// 		if x%2 != 0 {
+// 			return false
+// 		}
+// 		x /= 2
+// 	}
+// 	return x == 1
 // }
-// func main(){
-// 	res := isPowerof(128, 2)
-// 	fmt.Println(res)
+// func main() {
+// 	args := os.Args[1:]
+// 	if len(args) != 1 {
+// 		return
+// 	}
+// 	n, err := Atoi(args[0])
+// 	if err != nil {
+// 		fmt.Printf("%v\n", err)
+// 		return
+// 	} else {
+// 		res := isPowerof2(n)
+// 		fmt.Println(res)
+// 	}
+
 // }
+
+// ###################################    INTER  ###########################
 // func main(){
 // 	args := os.Args[1:]
 // 	if len(args) != 2 {
@@ -76,14 +244,14 @@ import (
 // 	var letters1 map[rune]bool = make(map[rune]bool)
 // 	for _, v := range s1 {
 // 		letters1[v] = true
-		
+
 // 	}
-	// var letters2 map[rune]bool = make(map[rune]bool)
-	// for _, v := range s2 {
-	// 	if letters1[v] && !letters2[v]{
-	// 		letters2[v] = true
-	// 	}
-	// }
+// 	var letters2 map[rune]bool = make(map[rune]bool)
+// 	for _, v := range s2 {
+// 		if letters1[v] && !letters2[v]{
+// 			letters2[v] = true
+// 		}
+// 	}
 // 	for _, v := range s1 {
 // 		if letters1[v]{
 // 			z01.PrintRune(v)
@@ -92,54 +260,7 @@ import (
 // 	}
 // 	z01.PrintRune('\n')
 // }
-// func main() {
-// 	args := os.Args[1:]
-// 	if len(args) != 2 {
-// 		return
-// 	}
-// 	s1 := args[0]
-// 	s2 := args[1]
-// 	var letters map[rune]bool = make(map[rune]bool)
-// 	for _, v := range s1 {
-// 		letters[v] = true
-// 	}
-// 	res := ""
-// 	for _, v := range s2 {
-// 		if letters[v] {
-// 			letters[v] = false
-// 			res += string(v)
-// 		}
-// 	}
-// 	fmt.Println(res)
-// }
-
-// func main(){
-// args := os.Args[1:]
-// if len(args) != 2 {
-// 	return
-// }
-// s1:=args[0]
-// s2:=args[1]
-// var letters map[rune]bool= make(map[rune]bool)
-
-// for _, v := range s1 {
-// 	letters[v] = true
-// }
-// var letters2 map[rune]int = make(map[rune]int)
-// for _, v := range s2 {
-// 	if letters[v]{
-// 		letters2[v]++
-// 	}
-// }
-// for _, v := range s1 {
-// 	if count, ok := letters2[v] ; ok && count > 0 {
-// 		z01.PrintRune(v)
-// 		letters2[v]--
-// 	}
-// }
-// z01.PrintRune('\n')
-// }
-// @@@@@@@@#########################################################################3
+// @@@@@@@@###############################     SEARCH REPLACE ##########################################3
 // func main(){
 // 	args := os.Args[1:]
 // 	if len(args) != 3 {
@@ -173,7 +294,7 @@ import (
 // 	}
 // 	return false
 // }
-//###################################################################################333
+//##################################   FOLD INT   #################################################333
 // func FoldInt(f func(int, int)int, sl []int, ac int){
 // 	res := ac
 // 	for _, n := range sl {
@@ -203,7 +324,7 @@ import (
 // func Sub(a, b int)int {
 // 	return a - b
 // }
-// @##############################################################################
+// @############################     DOOP   ##################################################
 // func main(){
 // 	args := os.Args[1:]
 // 	if len(args) == 0 {
@@ -251,44 +372,8 @@ import (
 // 	}
 
 // }
-// func itoa(x int) string{
-// 	var isneg bool
-// 	result := ""
-// 	if x < 0 {
-// 		isneg = true
-// 		x = -x
-// 	}
-// 	for x > 0 {
-// 	    digit:= x % 10
-// 		result = string(rune('0' + digit)) + result
-// 		x /= 10
-// 	}
-// 	if isneg {
-// 		result = "-" + result
-// 	}
-// 	return result
-// }
-// func atoi(s string)(int, error){
-// 	var isneg bool
-// 	result := 0
-// 	for i, n := range s {
-// 		if i == 0 && n == '-'{
-// 			isneg = true
-// 		}else if (n < '0' || n > '9') {
-// 			err := "Invalid number for conversion"
-// 			return 0, fmt.Errorf("Error:%s", err)
 
-// 		}else{
-// 			digit := int(rune(n - '0'))
-// 			result = result*10 + digit
-// 		}
-// 	}
-// 	if isneg {
-// 		result = -result
-// 	}
-// 	return result, nil
-// }
-// @#################################################################################
+// @######################################     ROT 13    ###########################################
 
 // func main() {
 // 	s := os.Args[1]
@@ -313,6 +398,168 @@ import (
 
 // 	}
 // 	return res
+// }
+// }
+
+// ########################################   COMPARE    ############################################3
+// func compare(s1, s2 string) int {
+// 	for i := 0; i < min(len(s1), len(s2)); i++ {
+// 		if s1[i] < s2[i] {
+// 			return -1
+// 		} else if s1[i] > s2[i] {
+// 			return 1
+// 		}
+// 	}
+// 	if len(s1) < len(s2) {
+// 		return -1
+// 	} else if len(s1) > len(s2) {
+// 		return 1
+// 	} else {
+// 		return 0
+// 	}
+// }
+
+// func min(x, y int) int {
+// 	if x < y {
+// 		return x
+// 	}
+// 	return y
+// }
+
+// func main() {
+// 	fmt.Println(compare("Hello!", "Hello!"))
+// 	fmt.Println(compare("Salut!", "lut!"))
+// 	fmt.Println(compare("Ola!", "Ol"))
+// }
+// ################################# ALPHA MIRROR   #################################################3
+// func alphaMir(){
+// 	args := os.Args[1:]
+// 	if len(args) == 0 {
+// 		return
+// 	}
+// 	s := args[0]
+// 	var str string
+// 	for _, v := range s {
+// 		if (v >= 'A' && v <= 'Z')  {
+// 			v = 'A' + ('Z' - v)
+// 		}else if (v >= 'a' && v <= 'z') {
+// 			v = 'a' + ('z' - v)
+// 		}
+// 		str += string(v)
+// 	}
+// 	fmt.Println(str)
+// }
+// func main(){
+// 	alphaMir()
+// }
+
+// ############################################   REDUCE INT ####################################3
+// func ReduceInt(a []int, f func(int, int) int) {
+// 	var z int
+//  if len(a) > 0 {
+// 	x := a[0]
+// 	y := a[1]
+// 	z = f(x, y)
+//  }
+//   s := Itoa(z)
+//   for _, v := range s {
+// 	z01.PrintRune(v)
+//   }
+//   z01.PrintRune('\n')
+// }
+// func Itoa(n int)string{
+// 	var isNeg bool
+// 	if n < 0 {
+// 		isNeg = true
+// 		n = -n
+// 	}
+// 	result := ""
+// 	for n > 0 {
+// 		digit := n % 10
+// 		result = string(rune('0' + digit)) + result
+// 		n /= 10
+// 	}
+// 	if isNeg {
+// 		result = "-"+result
+// 	}
+// 	return result
+// }
+
+// ###################################    CHUNK     #############################################3
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func Chunk(sl []int, sz int) {
+// 	var sli1 [][]int
+// 	var sli2 []int
+// 	if sz == 0 {
+// 		fmt.Println()
+// 	} else {
+// 		count := 0
+// 		for i := 0; i < len(sl); i++ {
+// 			sli2 = append(sli2, sl[i])
+// 			count++
+// 			if count == sz {
+// 				count = 0
+// 				sli1 = append(sli1, sli2)
+// 				sli2 = nil
+// 			}
+
+// 		}
+// 		if sli2 != nil {
+// 			sli1 = append(sli1, sli2)
+// 		}
+// 		fmt.Println(sli1)
+// 	}
+
+// }
+// func main() {
+// 	Chunk([]int{}, 10)
+// 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
+// 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
+// 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
+// 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
+// }
+
+// #############################3 ITOA ATOI #######################
+// func Itoa(n int) string {
+// 	var isNeg bool
+// 	if n < 0 {
+// 		isNeg = true
+// 		n = -n
+// 	}
+// 	result := ""
+// 	for n > 0 {
+// 		digit := n % 10
+// 		result = string(rune('0'+digit)) + result
+// 		n /= 10
+// 	}
+// 	if isNeg {
+// 		result = "-" + result
+// 	}
+// 	return result
+// }
+
+// func Atoi(s string) (int, error) {
+// 	var isneg bool
+// 	result := 0
+// 	for i, v := range s {
+// 		if i == 0 && v == '-' {
+// 			isneg = true
+// 		} else if v < '0' || v > '9' {
+// 			return 0, fmt.Errorf("error converting; invalid number")
+// 		} else if v >= '0' && v <= '9' {
+// 			digit := int(v - '0')
+// 			result = result*10 + digit
+// 		}
+// 	}
+// 	if isneg {
+// 		result = -result
+// 	}
+// 	return result, nil
 // }
 // ####################################################################
 // func main(){
@@ -379,280 +626,3 @@ import (
 // 	}
 
 // }
-// func itoa(x int)string{
-// 	var isNeg bool
-// 	result := ""
-// 	if x < 0 {
-// 		isNeg = true
-// 		x = -x
-// 	}else if x == 0 {
-// 		return "0"
-// 	}
-// 	for x > 0 {
-// 		digit := x%10
-// 		result =  string(rune('0' + digit))+ result
-// 		x /=10
-// 	}
-// 	if isNeg{
-// 		result = "-"+result
-// 	}
-// 	return result
-// }
-// ####################################################################################3
-// func compare(s1, s2 string) int {
-// 	for i := 0; i < min(len(s1), len(s2)); i++ {
-// 		if s1[i] < s2[i] {
-// 			return -1
-// 		} else if s1[i] > s2[i] {
-// 			return 1
-// 		}
-// 	}
-// 	if len(s1) < len(s2) {
-// 		return -1
-// 	} else if len(s1) > len(s2) {
-// 		return 1
-// 	} else {
-// 		return 0
-// 	}
-// }
-
-// func min(x, y int) int {
-// 	if x < y {
-// 		return x
-// 	}
-// 	return y
-// }
-
-// func main() {
-// 	fmt.Println(compare("Hello!", "Hello!"))
-// 	fmt.Println(compare("Salut!", "lut!"))
-// 	fmt.Println(compare("Ola!", "Ol"))
-// }
-// ##################################################################################3
-// func alphaMir(){
-// 	args := os.Args[1:]
-// 	if len(args) == 0 {
-// 		return
-// 	}
-// 	s := args[0]
-// 	var str string
-// 	for _, v := range s {
-// 		if (v >= 'A' && v <= 'Z') && (v + 25) > 'Z' {
-// 			v = 'A' + ('Z' - v)
-// 			str += string(v)
-// 		}else if (v >= 'a' && v <= 'z') && (v + 25) > 'z' {
-// 			v = 'a' + ('z' - v)
-// 			str += string(v)
-// 		}else if (v < 'A' || v > 'Z') || v == ' ' {
-// 			str += string(v)
-
-// 		}
-
-// 	}
-// 	fmt.Println(str)
-// }
-// func main(){
-// 	alphaMir()
-// }
-// "os"
-// "fmt"
-// ################################################################################3
-// func ReduceInt(a []int, f func(int, int) int) {
-// 	var z int
-//  if len(a) > 0 {
-// 	x := a[0]
-// 	y := a[1]
-// 	z = f(x, y)
-//  }
-//   s := Itoa(z)
-//   for _, v := range s {
-// 	z01.PrintRune(v)
-//   }
-//   z01.PrintRune('\n')
-// }
-// func Itoa(n int)string{
-// 	var isNeg bool
-// 	if n < 0 {
-// 		isNeg = true
-// 		n = -n
-// 	}
-// 	result := ""
-// 	for n > 0 {
-// 		digit := n % 10
-// 		result = string(rune('0' + digit)) + result
-// 		n /= 10
-// 	}
-// 	if isNeg {
-// 		result = "-"+result
-// 	}
-// 	return result
-// }
-// #########################################################################3
-// func Atoi(s string)int {
-// 	var isneg bool
-// 	result := 0
-// 	for i, v := range s {
-// 		if i == 0 && v == '-' {
-// 			isneg = true
-// 		}else if (v >= '0' && v <= '9') {
-// 			digit := int(v - '0')
-// 			result = result*10 + digit
-// 		}
-// 	}
-// 	if isneg {
-// 		result = -result
-// 	}
-// 	return result
-// }
-// func main(){
-// 	s := "-123"
-// 	r := Atoi(s)
-// 	fmt.Printf("result ni .. %d\n", r)
-// }
-// ###################################################################################
-// func main(){
-//  	args := os.Args[1:]
-// 	if len(args) == 0{
-// 		return
-// 	}
-// 	s1 := args[0]
-// 	res := Rot13(s1)
-// 	for _, v := range res {
-// 		z01.PrintRune(v)
-// 	}
-// 	z01.PrintRune('\n')
-// }
-// func Rot13(s string)string{
-// 	var s0 string
-// 	for _, v := range s {
-// 		if (v >= 'a' && v <= 'z') &&  (v + 13) > 'z'{
-// 			v = (v + 13) - 26
-// 			s0 += string(v)
-// 		}else if (v >= 'a' && v <= 'z') &&  (v + 13) <= 'z' {
-// 			v = (v + 13)
-// 			s0 += string(v)
-// 		}else if (v >= 'A' && v <= 'Z') &&  (v + 13) > 'Z'{
-// 			v = (v + 13) - 26
-// 			s0 += string(v)
-// 		}else if (v >= 'A' && v <= 'Z') &&  (v + 13) <= 'Z' {
-// 			v = (v + 13)
-// 			s0 += string(v)
-// 		}else if (v < 'A' || v > 'Z') || v == ' ' {
-// 			s0 += string(v)
-// 		}
-// 	}
-// 	return s0
-// }
-// ################################################################################3
-// package main
-
-// import (
-// 	"fmt"
-// )
-
-// func Chunk(sl []int, sz int) {
-// 	var sli1 [][]int
-// 	var sli2 []int
-// 	if sz == 0 {
-// 		fmt.Println()
-// 	} else {
-// 		count := 0
-// 		for i := 0; i < len(sl); i++ {
-// 			sli2 = append(sli2, sl[i])
-// 			count++
-// 			if count == sz {
-// 				count = 0
-// 				sli1 = append(sli1, sli2)
-// 				sli2 = nil
-// 			}
-
-// 		}
-// 		if sli2 != nil {
-// 			sli1 = append(sli1, sli2)
-// 		}
-// 		fmt.Println(sli1)
-// 	}
-
-// }
-// func main() {
-// 	Chunk([]int{}, 10)
-// 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
-// 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
-// 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
-// 	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
-// }
-
-// ############################################################################3
-// package main
-
-// import (
-// 	"fmt"
-// 	"strconv"
-// )
-
-// func main() {
-
-// }
-// func sumDig(x int) int {
-// 	s := Itoa(x)
-// 	sum := 0
-// 	for i := 0; i < len(s); i++ {
-// 		b := Atoi(string(s[i]))
-// 		sum += b
-// 	}
-// 	return sum
-// }
-// func digitalRoot(n int)int {
-// 	if n < 10 {
-// 		return n
-// 	}
-// 	return digitalRoot(sumDig(n))
-// }
-
-// func main() {
-// 	args := os.Args[1:]
-// 	if len(args) == 0 {
-// 		return
-// 	}
-// 	s1 := args[0]
-// 	x := Atoi(s1)
-// 	r := digitalRoot(x)
-// 	fmt.Printf(" ...ni..%d\n", r)
-// }
-
-func Itoa(n int) string {
-	var isNeg bool
-	if n < 0 {
-		isNeg = true
-		n = -n
-	}
-	result := ""
-	for n > 0 {
-		digit := n % 10
-		result = string(rune('0'+digit)) + result
-		n /= 10
-	}
-	if isNeg {
-		result = "-" + result
-	}
-	return result
-}
-
-func Atoi(s string) (int, error) {
-	var isneg bool
-	result := 0
-	for i, v := range s {
-		if i == 0 && v == '-' {
-			isneg = true
-		} else if v < '0' && v > '9'{
-			return 0, fmt.Errorf("error converting; invalid number")
-		}else if v >= '0' && v <= '9' {
-			digit := int(v - '0')
-			result = result*10 + digit
-		}
-	}
-	if isneg {
-		result = -result
-	}
-	return result, nil
-}
